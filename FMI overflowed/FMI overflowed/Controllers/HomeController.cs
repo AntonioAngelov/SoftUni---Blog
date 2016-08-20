ded6339a36e69e3ae8def8bff6d6f009efcc415a
AntonioAngelov <antonio.angelov1@gmail.com>
2016-08-20 13:41:31 +0300

    getting started with the forum

---
+++ FMI overflowed/FMI overflowed/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FMI_overflowed.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Post()
+        {
+            return View();
+        }
+
+        public ActionResult FunPost()
+        {
+            return View();
+        }
+
+    }
+}
