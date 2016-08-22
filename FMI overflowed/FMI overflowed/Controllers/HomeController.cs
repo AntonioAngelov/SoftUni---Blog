41ebbf93e1faaf78b5376084da8fc92e582bad3a
AntonioAngelov <antonio.angelov1@gmail.com>
2016-08-22 16:36:16 +0300

    now it displays the most ecent post on the homepage

--- FMI overflowed/FMI overflowed/Controllers/HomeController.cs
+++ FMI overflowed/FMI overflowed/Controllers/HomeController.cs
@@ -4,15 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace FMI_overflowed.Controllers
 {
     public class HomeController : Controller
     {
+        //paste the db from where 
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
-            return View();
-
+            var latestPost = db.Post.Include(p => p.Author).OrderByDescending(p => p.Date).Take(1);
+            return View(latestPost);
         }
 
         public ActionResult MathPosts()
